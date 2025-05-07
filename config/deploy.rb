307fe93d225edecc20a9386cc7870f8178b068bd
imadbourouche <ji_bourouche@esi.dz>
2025-05-07 17:18:45 +0200

    change deploy.rb configurations

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,11 +14,11 @@ set :deploy_to, "/opt/ontoportal/ncbo_cron"
 set :log_level, :debug
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml", 'config/master.key'
+append :linked_files, "config/config.rb"
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-set :linked_dirs, %w{log vendor/bundle tmp/pids tmp/sockets public/system}
+set :linked_dirs, %w{logs vendor/bundle tmp/pids tmp/sockets public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -39,14 +39,14 @@ set :config_folder_path, "#{fetch(:application)}/#{fetch(:stage)}"
 
 set :ssh_options, {
   user: 'ontoportal',
-  keys: %w(config/deploy_id_rsa),
-  auth_methods: %w(publickey),
-#   forward_agent: 'true',
-#   proxy: Net::SSH::Proxy::Command.new("ssh #{JUMPBOX_PROXY} -W %h:%p")
+  # keys: %w(config/deploy_id_rsa),
+  # auth_methods: %w(publickey),
+  # forward_agent: 'true',
+  # proxy: Net::SSH::Proxy::Command.new("ssh #{JUMPBOX_PROXY} -W %h:%p")
 }
 
 # private git repo for configuraiton
-PRIVATE_CONFIG_REPO = ENV.include?('PRIVATE_CONFIG_REPO') ? ENV['PRIVATE_CONFIG_REPO'] : 'https://your_github_pat_token@github.com/your_organization/ontoportal-configs.git'
+# PRIVATE_CONFIG_REPO = ENV.include?('PRIVATE_CONFIG_REPO') ? ENV['PRIVATE_CONFIG_REPO'] : "https://your_github_pat_token@github.com/#{fetch(:author)}/ontoportal-configs.git"
 
 # Smoke test for checking if the service is up
 desc 'Smoke test: Check if ncbo_cron service is running'
@@ -64,36 +64,31 @@ end
 
 namespace :deploy do
 
-  desc 'Incorporate the private repository content'
-  # Get cofiguration from repo if PRIVATE_CONFIG_REPO env var is set
-  # or get config from local directory if LOCAL_CONFIG_PATH env var is set
-  task :get_config do
-    if defined?(PRIVATE_CONFIG_REPO)
-      TMP_CONFIG_PATH = "/tmp/#{SecureRandom.hex(15)}".freeze
-      on roles(:app) do
-        execute "git clone -q #{PRIVATE_CONFIG_REPO} #{TMP_CONFIG_PATH}"
-        execute "rsync -av #{TMP_CONFIG_PATH}/#{fetch(:config_folder_path)}/ #{release_path}/"
-        execute "rm -rf #{TMP_CONFIG_PATH}"
-      end
-    elsif defined?(LOCAL_CONFIG_PATH)
-      on roles(:app) do
-        execute "rsync -av #{LOCAL_CONFIG_PATH}/#{fetch(:application)}/ #{release_path}/"
-      end
-    end
-  end
+  # desc 'Incorporate the private repository content'
+  # # Get cofiguration from repo if PRIVATE_CONFIG_REPO env var is set
+  # # or get config from local directory if LOCAL_CONFIG_PATH env var is set
+  # task :get_config do
+  #   if defined?(PRIVATE_CONFIG_REPO)
+  #     TMP_CONFIG_PATH = "/tmp/#{SecureRandom.hex(15)}".freeze
+  #     on roles(:app) do
+  #       execute "git clone -q #{PRIVATE_CONFIG_REPO} #{TMP_CONFIG_PATH}"
+  #       execute "rsync -av #{TMP_CONFIG_PATH}/#{fetch(:config_folder_path)}/ #{release_path}/"
+  #       execute "rm -rf #{TMP_CONFIG_PATH}"
+  #     end
+  #   elsif defined?(LOCAL_CONFIG_PATH)
+  #     on roles(:app) do
+  #       execute "rsync -av #{LOCAL_CONFIG_PATH}/#{fetch(:application)}/ #{release_path}/"
+  #     end
+  #   end
+  # end
 
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
       execute 'sudo systemctl restart ncbo_cron.service'
-      execute 'sleep 5'
     end
   end
 
-  after :updating, :get_config
+  # after :updating, :get_config
   after :publishing, :restart
-  after :restart, :smoke_test
-
 end
